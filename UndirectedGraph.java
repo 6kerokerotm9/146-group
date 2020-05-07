b719e16f5e35fd14bd9bc0d90ed90249dbeeadfa
unknown <luckystripes60@gmail.com>
2020-05-06 20:39:33 -0700

    first commit

---
+++ UndirectedGraph.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class UndirectedGraph<AnyType> {
+    private ArrayList<ArrayList<AnyType>> matrix; //2d array where the 0 index of each list is the original profile and the rest are friends
+    
+    public UndirectedGraph() {
+        matrix = new ArrayList<>();
+    }
+    
+    public void addVertex(AnyType t) {
+        ArrayList<AnyType> temp = new ArrayList<>();
+        temp.add(t);
+        matrix.add(temp);
+        int index = matrix.indexOf(t);
+        makeEdge(t, index);
+    }
+    
+    public void makeEdge(AnyType t, int index) {
+        ArrayList<AnyType> temp = matrix.get(index);
+        for(AnyType e : ) {
+            
+        }
+    }
+}
